435b59c0f32bf7ca0cdcfc90e0fac925749cd579
felix-rojas <a01201946@tec.mx>
2023-06-15 11:29:28 -0600

    window behavior

--- src/platform/win32_platform.cpp
+++ src/platform/win32_platform.cpp
@@ -1,4 +1,5 @@
 #include <windows.h>
+#include "renderer/vk_renderer"
 
 static bool is_running = true;
 
@@ -21,6 +22,7 @@ bool platform_create_window(HWND window)
     wc.lpfnWndProc = platform_window_callback;
     wc.hInstance = instance;
     wc.lpszClassName = "vulkan_window";
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
 
     if (!RegisterClassA(&wc))
     {
@@ -31,8 +33,9 @@ bool platform_create_window(HWND window)
     window = CreateWindowExA(
         WS_EX_APPWINDOW,
         "vulkan_window",
-        "Pong",
-        0, 100, 100, 1600, 720, 0, 0, instance, 0);
+        "vulkan_app",
+        WS_THICKFRAME | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_OVERLAPPED,
+        100, 100, 1600, 720, 0, 0, instance, 0);
 
     if (window == 0)
     {
@@ -41,19 +44,37 @@ bool platform_create_window(HWND window)
     }
 
     ShowWindow(window, SW_SHOW);
+    return true;
+}
+
+void platform_update_window(HWND window)
+{
+    MSG msg;
+    while (PeekMessageA(&msg, window, 0,0, PM_REMOVE))
+    {
+        TranslateMessage(&msg);
+        DispatchMessageA(&msg);
+    }
+    
 }
 
 int main()
 {
+    VkContext vkcontext = {};
     HWND window = 0;
-    if (platform_create_window(window))
+    if (!platform_create_window(window))
+    {
+        return -1;
+    }
+
+    if (!vk_init(&vkcontext))
     {
         return -1;
     }
 
     while (is_running)
     {
-        /* code */
+        platform_update_window(window);
     }
 
     return 0;
