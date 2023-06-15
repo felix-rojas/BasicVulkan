9b3592947bf1ced1329e2f6c07b9cdadd246920d
felix-rojas <a01201946@tec.mx>
2023-06-15 11:08:49 -0600

    debugged error

--- src/platform/win32_platform.cpp
+++ src/platform/win32_platform.cpp
@@ -9,9 +9,6 @@ LRESULT CALLBACK platform_window_callback(HWND window, UINT msg, WPARAM wParam, 
     case WM_CLOSE:
         is_running = false;
         break;
-    
-    default:
-        break;
     }
 
     return DefWindowProcA(window, msg, wParam, lParam);
@@ -20,33 +17,31 @@ LRESULT CALLBACK platform_window_callback(HWND window, UINT msg, WPARAM wParam, 
 bool platform_create_window(HWND window)
 {
     HINSTANCE instance = GetModuleHandleA(0);
-    WNDCLASS wc;
+    WNDCLASSA wc = {};
     wc.lpfnWndProc = platform_window_callback;
     wc.hInstance = instance;
     wc.lpszClassName = "vulkan_window";
 
-    if(!RegisterClassA(&wc))
+    if (!RegisterClassA(&wc))
     {
         MessageBoxA(window, "Failed registering window class", "Error", MB_ICONEXCLAMATION | MB_OK);
         return false;
-    } 
+    }
 
-    CreateWindowExA(
+    window = CreateWindowExA(
         WS_EX_APPWINDOW,
         "vulkan_window",
         "Pong",
-        0,100,100,1600,720,0,0,instance,0
-    );
+        0, 100, 100, 1600, 720, 0, 0, instance, 0);
 
     if (window == 0)
     {
         MessageBoxA(window, "Failed creating window", "Error", MB_ICONEXCLAMATION | MB_OK);
-        return false;   
+        return false;
     }
 
-    ShowWindow(window,SW_SHOW);
+    ShowWindow(window, SW_SHOW);
 }
-
 
 int main()
 {
@@ -55,5 +50,11 @@ int main()
     {
         return -1;
     }
+
+    while (is_running)
+    {
+        /* code */
+    }
+
     return 0;
 }
