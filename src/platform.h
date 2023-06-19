ff772789289114abf7e7947bd646bb098530f66a
felix-rojas <a01201946@tec.mx>
2023-06-19 08:44:43 -0600

    add file reading windows

--- src/platform.h
+++ src/platform.h
@@ -1,3 +1,4 @@
 #pragma once
 
 void platform_get_window_size(uint32_t *pWidth, uint32_t *pHeight);
+char* platform_read_file(char* file_path, uint32_t* file_length);
--- src/platform/win32_platform.cpp
+++ src/platform/win32_platform.cpp
@@ -3,6 +3,9 @@
 #include "defines.h"
 #include "platform.h"
 
+using std::cout;
+using std::endl;
+
 // global variable is just static type but makes it easier to understand
 global_variable bool is_running = true;
 global_variable HWND window;
@@ -93,5 +96,45 @@ void platform_get_window_size(uint32_t *pWidth, uint32_t *pHeight)
     GetClientRect(window, &rect);
 
     *pWidth = rect.right - rect.left;
-    *pHeight =      rect.bottom - rect.top;
+    *pHeight = rect.bottom - rect.top;
+}
+
+char *platform_read_file(char *file_path, uint32_t *file_length)
+{
+    char *result = 0;
+    LPCSTR path = (LPCSTR)file_path; 
+
+    HANDLE win_file = CreateFileA(path, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, 0, 0);
+
+    if (win_file != INVALID_HANDLE_VALUE)
+    {
+        LARGE_INTEGER size;
+        if (GetFileSizeEx(win_file, &size))
+        {
+            result = new char[size.QuadPart];
+            DWORD bytesRead;
+            if (ReadFile(win_file, result, size.QuadPart, &bytesRead, 0))
+            {
+                // SUCCESS!
+            }
+            else
+            {
+                /* code */
+            }
+        }
+        else
+        {
+            // ASSERT
+            cout << "Failed to get file size " << endl;
+        }
+        CloseHandle(win_file);
+    }
+
+    else
+    {
+        // ASSERT
+        cout << "Failed to open file " << endl;
+    }
+
+    return result;
 }
